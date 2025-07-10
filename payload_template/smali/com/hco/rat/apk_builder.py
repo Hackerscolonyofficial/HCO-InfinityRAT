5324c9153a841578483798047dd914f88e0d5a6e
Hackers Colony Official <103638018+Hackerscolonyofficial@users.noreply.github.com>
2025-07-10 16:32:49 +0530

    Delete payload_template/smali/com/hco/rat/apk_builder.py

--- payload_template/smali/com/hco/rat/apk_builder.py
+++
@@ -1,8 +0,0 @@
-import os
-
-print("[+] Building HCO-InfinityRAT APK...")
-
-# Build the APK from the payload_template folder
-os.system("apktool b payload_template -o output/HCO-InfinityRAT.apk")
-
-print("[+] APK built successfully at: output/HCO-InfinityRAT.apk")
